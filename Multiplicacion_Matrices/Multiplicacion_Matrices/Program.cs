4cdbed131f14f810f469b984aa20eb1a76d2072e
Sebastián Acevedo <soyelsebas613@gmail.com>
2025-09-18 12:20:27 -0500

    creation of both solutions

---
+++ Multiplicacion_Matrices/Multiplicacion_Matrices/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("Ingrese el valor de m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor de p: ");
+        int p = int.Parse(Console.ReadLine());
+
+        int[,] A = new int[m, n];
+        int[,] B = new int[n, p];
+        int[,] C = new int[m, p];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                A[i, j] = (i + 1) * j;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                B[i, j] = (j + 1) * i;
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                C[i, j] = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    C[i, j] += A[i, k] * B[k, j];
+                }
+            }
+        }
+
+        Console.WriteLine("\n*** A ***");
+        PrintMatrix(A);
+
+        Console.WriteLine("\n*** B ***");
+        PrintMatrix(B);
+
+        Console.WriteLine("\n*** C ***");
+        PrintMatrix(C);
+    }
+
+    private static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Rombo/Rombo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("Ingrese el tamaño del rombo: ");
+        if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+        {
+            Console.WriteLine("Ingrese un entero positivo.");
+            return;
+        }
+
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Por favor ingrese un número impar.");
+            return;
+        }
+
+        int mid = n / 2;
+
+        for (int i = 0; i <= mid; i++)
+        {
+            Console.Write(new string(' ', mid - i));
+
+            if (i == 0)
+            {
+                Console.WriteLine("#");
+            }
+            else
+            {
+                Console.Write("#");
+                Console.Write(new string(' ', 2 * i - 1));
+                Console.WriteLine("#");
+            }
+        }
+
+        for (int i = mid - 1; i >= 0; i--)
+        {
+            Console.Write(new string(' ', mid - i));
+            if (i == 0)
+                Console.WriteLine("#");
+            else
+            {
+                Console.Write("#");
+                Console.Write(new string(' ', 2 * i - 1));
+                Console.WriteLine("#");
+            }
+        }
+    }
+}
